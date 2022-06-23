f7926c1436eb1a9838d5285309641f17fe1e68b1
Brandon Smith <brandon.kev.smith@gmail.com>
2022-06-22 19:09:30 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Brandon Smith on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Landmarks/Views/LandmarkDetails.swift
@@ -0,0 +1,57 @@
+//
+//  LandmarkDetails.swift
+//  Landmarks
+//
+//  Created by Brandon Smith on 6/22/22.
+//
+
+import SwiftUI
+
+struct LandmarkDetails: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                
+                Text(landmark.name)
+                    .font(.title)
+                    .foregroundColor(.black)
+                
+                HStack {
+                    Text(landmark.park)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+                
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetails(landmark: landmarks[0])
+    }
+}
