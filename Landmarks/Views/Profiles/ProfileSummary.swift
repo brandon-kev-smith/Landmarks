9dc645a8a10b8cd10a0997187afbcbd93a22484e
Brandon Smith <brandon.kev.smith@gmail.com>
2022-07-07 22:19:10 -0500

    Added a profile that can be adjusted to the user.

---
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,61 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Brandon Smith on 7/7/22.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    @EnvironmentObject var modelData: ModelData
+    var profile: Profile
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .font(.title)
+                    .bold()
+                Text("Notifications: \(profile.prefrersNotifications ? "on" : "off")")
+                Text("Seasonal Photo: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                
+                Divider()
+
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+
+                    HikeView(hike: modelData.hikes[0])
+                }
+            }
+        }
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary(profile: Profile.default)
+            .environmentObject(ModelData())
+    }
+}
